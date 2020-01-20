59f0767e0908b46a194cfe0776c7be029eb272af
Ego <47955681+egocious@users.noreply.github.com>
2020-01-20 16:48:49 +0000

    amend file

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 
 data_structure = {'toke.makinwa@yahoo.com': 102000 , 'sandra.orji@yahoo.com': 12000, 'mohammed.buhari@yahoo.com': 44000, 'shola.badmus@yahoo.com': 49000, 'pius. sydney@yahoo.com': 72000,  'shola.shobowale@yahoo.com': 56000, 'abigail.turko@yahoo.com':592000, 'teni.lawal@yahoo.com': 81000, 'bambam.ortega@yahoo.com': 84000, 'darlington.yaki@yahoo.com': 72000, 'yemi.osibajo@yahoo.com': 37000 }
 
-option = raw_input("Press 1 to create an account \nPress 2 for transactions: ")
+option = raw_input("Press 1 \nTo create an account \nPress 2 \nFor transactions: ")
 
 if option == "1":
     name = raw_input("Enter your name: ")
@@ -11,7 +11,6 @@ if option == "1":
     for key in data_structure:  
         if key == email:
             print "You provided an invalid input"
-            #*return option  this did not go to option ... how do i send it back to the top?
         else:
             print "Account created"
 
@@ -19,7 +18,7 @@ elif option == "2":
     name = raw_input("Enter your name: ")
     password1 = raw_input("Enter your 6 letter password: ")
     if len(password1) == 6:
-        print "Welcome 1" + name
+        print "Welcome " + name
         print "Press 1 to check balance"
         print "Press 2 to deposit"
         print "Press 3 to withdraw"
@@ -27,10 +26,11 @@ elif option == "2":
     elif len(password1) > 6:
         print "You are not authorised"
         print "Please create an account with us"
-        #*return options did not eeturn
+        name = raw_input("Enter your name: ")
+        password1 = raw_input("Enter your 6 letter password: ")
     else:
         print "Password is too short"
-        print password1 #this did not go to passwod1 . went to next line instead
+        
 
     options = raw_input("Press: ")
 
@@ -39,18 +39,18 @@ elif option == "2":
         current_balance = 12222
         print current_balance
         print "Thank you for banking with us"
-    elif options == "2":
-        #users_balance 
+
+    elif options == "2": 
         deposit = int(raw_input("Enter deposit amount >> "))
-        new_balance = deposit + current_balance #*not defined
+        new_balance = deposit + current_balance 
         print "New balance is: %d" %new_balance
         print "Thank you for banking with us" 
+        
     elif options == "3":
         withdrawal = int(raw_input("Enter withdrawal amount >> "))
-        new_wbalance = new_balance - withdrawal#* new bal not defined
+        new_wbalance = new_balance - withdrawal
         if new_balance == 0:
             print "Please deposit some funds"
-            #*return deposit
         elif new_balance > 0:
             print "Amount withdrawn: " + withdrawal
             print "New balance is " + new_wbalance
@@ -61,10 +61,8 @@ elif option == "2":
     elif options == "4":
         transfer_email = raw_input("Input beneficiary's email >> ")
         transfer_amount = int(raw_input("Input transfer amount >> "))
-        #curent_balance = 10000
         beneficiary_balance = 0
         new_ubalance = new_wbalance - transfer_amount
-        #add the amount to the beneficiaries account,
         if transfer_amount == 0:
             print "Please input an amount"
         elif transfer_amount > 0:
